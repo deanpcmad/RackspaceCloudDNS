570655636d12a0d0daa0b6f50eb7cd023745b975
Dean Perry <dean@deanperry.net>
2013-03-03 15:22:50 +0000

    fixed domain record edit

--- lib/rackspace_cloud_dns/record.rb
+++ lib/rackspace_cloud_dns/record.rb
@@ -18,14 +18,14 @@ module RackspaceCloudDns
 
 		# Create a record on a domain
 		def self.create(domain_id, name, type, data, priority=nil)
-			options = {:records => [:name => "#{name}", :type => type, :data => data, :priority => priority]}
+			options = {:records => [:name => name, :type => type, :data => data, :priority => priority]}
 
 			RackspaceCloudDns::Request.request("domains/#{domain_id}/records", :post, options)
 		end
 
 		# Edit a record for a domain
-		def self.edit(domain_id, record_id, name, data, priority)
-			options = {:records => [:name => "#{name}", :data => data, :priority => priority]}
+		def self.edit(domain_id, record_id, name, data, priority=nil)
+			options = {:name => name, :data => data, :priority => priority}
 
 			RackspaceCloudDns::Request.request("domains/#{domain_id}/records/#{record_id}", :put, options)
 		end
